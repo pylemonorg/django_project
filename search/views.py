bfc6928e28c334f2d86e66e95524e9d2ce8ce92b
pylemonorg <pylemonorg@gmail.com>
2019-06-10 14:49:01 +0800

    updata

--- search/views.py
+++ search/views.py
@@ -1,27 +1,61 @@
+import os
 import json
-
+import csv
+from random import randint
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import *
 # Create your views here.
 
+# 读取CSV文件模版信息
+csv_data = {"json_data": []}
+csv_file = os.path.join(os.path.dirname(os.path.dirname(__file__)), r'static/rule_files/all.csv')
+with open(csv_file, 'r', encoding='utf-8-sig') as f:
+    _csv_data = csv.DictReader(f)
+    for _i in _csv_data:
+        csv_data['json_data'].append(list(dict(_i).values()))
+
 
 class IndexView(View):
 
     def get(self, request):
+        # 背景图片下标
+        # bg_index = randint(1, 5)
         return render(request, 'index.html')
 
     def post(self, request):
-        print(request.POST)
         request_data = dict(request.POST)
-        search_msg = request_data.get('search_msg', '')
-        print(search_msg)
+        search_msg = request_data.get('search_msg',)  # 列表[]
+        search_msg = search_msg[0].strip()
+        print('请求内容：', search_msg)
+        if not search_msg:
+            response_data = {'json_data': None}
+            return HttpResponse(json.dumps(response_data))
+
         # print(request.text)
         # request_data = json.loads(request.body.decode())
         # request_data = request.POST()
         # print(request_data)
         # response_data = {'json_data': '123123132131'}
-        response_data = {'json_data': [['模版1', 'qq', 'qq.com'], ['模版2', 'taobao', 'taobao.com'], ]}
+        # json_data = {'json_data': []}
+        # for i in range(1, 30):
+        #     json_data["json_data"].append(['模版1', 'qq', 'qq.com'])
+        # # response_data = {'json_data': [['模版1', 'qq', 'qq.com'], ['模版2', 'taobao', 'taobao.com'], ]}
+        # json_data = {'json_data': None}
+        # 查找csv内容
+        csv_msg = csv_data.get("json_data")
+        response_data = {"json_data": []}
+        for csv_li in csv_msg:
+            for i in csv_li:
+
+                if search_msg in i:
+                    response_data["json_data"].append(csv_li)
+                    break
+                else:
+                    continue
+        if not response_data.get("json_data"):
+            response_data = {"json_data": None}
+        print('返回内容', response_data)
         return HttpResponse(json.dumps(response_data))
 
 
--- static/bootstrap-3.3.7-dist/js/index.js
+++ static/bootstrap-3.3.7-dist/js/index.js
@@ -1,27 +1,59 @@
 window.onload = function () {
-    // 修改背景大小
-    var html_width = document.body.clientWidth;
-    var html_height = window.innerHeight;
-    var body_height = document.body.scrollHeight;
-    if (html_height < body_height) {
-        html_height = body_height;
-    }
+    // 获取浏览器大小
+    // var html_width = document.body.clientWidth;
+    // var html_height = window.innerHeight;
+    // var body_height = document.body.scrollHeight;
+    // // 判断 浏览器最大的显示高度
+    // if (html_height < body_height) {
+    //     html_height = body_height;
+    // }
+    //
+    // // 更改背景大小
+    // console.log('页面高度：' + html_height);
+    // document.getElementById('de_canvas').style.height = html_height + 'px';
+    // // document.getElementById('de_canvas').style.width= html_width + 'px';
+    //
+    // // 搜索框页面垂直居中
+    // // del_height 减去多余的高度
+    // function div_position(del_height) {
+    //
+    //     $("#content_box").css({
+    //         position: "absolute",
+    //         left: ($(window).width() - $("#content_box").outerWidth()) / 2,
+    //         top: (html_height - $("#content_box").outerHeight()) / 2 - 100 - del_height
+    //     });
+    // }
+    //
+    //
+    // // $(window).resize(function () {
+    // //     $("#content_box").css({
+    // //         position: "absolute",
+    // //         left: ($(window).width() - $("#content_box").outerWidth()) / 2,
+    // //         top: (html_height - $("#content_box").outerHeight()) / 2 - 100
+    // //     });
+    // // });
+    // $(window).resize(div_position(0));
 
-    console.log(html_height);
-    document.getElementById('de_canvas').style.height = html_height + 'px';
-    // document.getElementById('de_canvas').style.width= html_width + 'px';
-
-    // 搜索框页面垂直居中
-    $(window).resize(function () {
-        $("#content_box").css({
-            position: "absolute",
-            left: ($(window).width() - $("#content_box").outerWidth()) / 2,
-            top: (html_height - $("#content_box").outerHeight()) / 2 - 100
-        });
-    });
-    $(window).resize();
-
+    // 点击搜索触发ajax
     $("#botton_go").click(function () {
+        var search_msg = $('#search_input').val();
+        search(search_msg)
+    });
+
+    // 失去焦点 触发ajax
+    $("#search_input").blur(function () {
+        var search_msg = $('#search_input').val();
+        search(search_msg)
+    });
+
+    // 失去焦点 触发ajax
+    $("#search_input").keyup(function () {
+        var search_msg = $('#search_input').val();
+        search(search_msg)
+    });
+
+    // 输入内容 触发ajax
+    $("#search_input").keyup(function () {
         var search_msg = $('#search_input').val();
         search(search_msg)
     });
@@ -43,18 +75,23 @@ window.onload = function () {
 
     //  请求成功执行的函数
     function suc(request) {
-        console.log(request);
+        // console.log(request);
         var json_data = JSON.parse(request)['json_data'];
 
         if (json_data == null) {
             // 没有数据
-            $('#search_show').show();
+            // $('#search_show').show();
             console.log('没有返回数据');
             nullshowserch();
 
+        } else if (json_data == '') {
+            // 没有数据
+            // $('#search_show').show();
+            console.log('没有返回数据');
+            nullshowserch();
         } else {
             // 展示数据
-            console.log('返回结果：\n' + json_data);
+            // console.log('返回结果：\n' + json_data);
             $('#search_show').show();
             showserch(json_data)
         }
@@ -62,25 +99,45 @@ window.onload = function () {
 
     // 展示数据内容
     function showserch(json_data) {
-        var _html = '<ul class="list-unstyled">';
+        // var _html = '<ol class="list-unstyled">';
+        var _html = '<ol>';
         for (var i = 0; i < json_data.length; i++) {
             var rule_name = json_data[i][0];  // 模版名字
-            var domain_name = json_data[i][1];  // 域名
-            var url = json_data[i][2];  // 域名地址
-            var li_msg = '<li>' + rule_name + '&nbsp;' + domain_name + url + '</li>';
+            var web_name = json_data[i][1];  // 网站名称
+            var domain_name = json_data[i][2];  // 域名
+            var url = json_data[i][3]; //链接
+            var li_msg = '<li class="text-muted"><strong><a href="' + url +
+                '">' + web_name +
+                '&nbsp;/&nbsp;' + domain_name + '</a></strong>&nbsp;&nbsp;&nbsp;' + rule_name + '</li>';
             _html += li_msg
         }
-        _html += '</ul>';
-        $('#search_show').show();
+        _html += '</ol>';
         $('#search_show').html(_html);
+        $('#search_show').show();
     }
 
     // 提示没有数据内容
     function nullshowserch() {
         var _html = '<span class="glyphicon glyphicon-exclamation-sign text-left" aria-hidden="true"></span>' +
             '<span class="sr-only text-left">Error:</span>' +
-            '&nbsp;很绝望&nbsp;ヾ(×× ) ﾂ，没搜索到！';
+            '&nbsp;NULL&nbsp;ヾ(×× ) ﾂ，没搜索到！';
         $('#search_show').html(_html);
         $('#search_show').css({'color': '#a94442'});
+        $('#search_show').show();
+
+        // var height_search_show = $('#search_show').innerHeight();  // 获取显示div的高度
+        // $(window).resize(div_position(height_search_show));  // 保持搜索所在位置
+
     }
+    // function switch_bg(){
+    //     var bg_index = Math.floor(Math.random()*5+1);
+    //     $('#de_canvas').attr('src', '/static/bootstrap-3.3.7-dist/js/a'+ bg_index +'.html')
+    //     // alert(bg_index)
+    // }
+    // setInterval(switch_bg, 1000)
 };
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     document.body.style.display = 'block';
+// });
+
